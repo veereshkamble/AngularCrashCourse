4e0cde1afdc81ae911f7e3ee16f6fda0eec2adce
veereshkamble <veereshkamble7@gmail.com>
2020-04-12 21:24:55 -0700

    Components and Todo object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = 'Veeresh';
+  name: string = 'Veeresh';
+
+  constructor() {
+    console.log(7);
+    this.changeName('Ronaldo'); 
+  }
+
+  changeName(name:string):void {
+    this.name = name;
+  }
 }
