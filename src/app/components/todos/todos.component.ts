7757666720fcf015a54e2d30b041043b28b6a5f4
veereshkamble <veereshkamble7@gmail.com>
2020-04-13 23:56:14 -0700

    Angular Crash Course - Event Emitter -  Monday, April 13, 2020

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -19,6 +19,9 @@ export class TodosComponent implements OnInit {
   }
 
   deleteTodo(todo:Todo) {
+    // Remove from UI
     this.todos= this.todos.filter(t => t.id !== todo.id);
+    //Remove from server
+    this.todoService.deleteTodo(todo).subscribe();
   }
 }
