14e161c8020260162ccc91d225907ccda4fab1f0
veereshkamble <veereshkamble7@gmail.com>
2020-04-13 23:36:47 -0700

    Angular Crash Course - Moday, April 13, 2020

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Todo } from 'src/app/models/Todo';
+import { TodoService } from '../../services/todo.service';
 
 @Component({
   selector: 'app-todo-item',
@@ -9,9 +10,30 @@ import { Todo } from 'src/app/models/Todo';
 export class TodoItemComponent implements OnInit {
   @Input() todo:Todo;
 
-  constructor() { }
+  constructor(private todoService:TodoService) { }
 
   ngOnInit() {
   }
 
+  // Set Dynamic Classes
+  setClasses() {
+    let classes = {
+      todo: true,
+      'is-complete': this.todo.completed
+    }
+    return classes;
+  }
+
+  onToggle(todo) {
+    console.log('toggle ' + this.todo.title);
+    // Toggle in UI
+    this.todo.completed = !this.todo.completed;
+    // Toggle on server
+    this.todoService.toggleCompleted(todo).subscribe(todo =>
+      console.log(todo)) ;
+  }
+
+  onDelete(todo) {
+    console.log('delete ' + this.todo.title);
+  }
 }
