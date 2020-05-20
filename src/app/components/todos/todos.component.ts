33a881c95fdfe4c92b9156f2b2933e9744e22cf8
veereshkamble <veereshkamble7@gmail.com>
2020-05-20 00:15:58 -0700

    Wednesday, May 20, 2020

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoService } from '../../services/todo.service';
-import { Todo } from '../../models/Todo';
+import { Todo } from '../../models/todo';
 
 @Component({
   selector: 'app-todos',
